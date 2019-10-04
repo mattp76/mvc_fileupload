c320fe47eeeae53168b25e496c438cc0ae8835f1
matparker <matparker@nine.com.au>
2019-10-04 14:41:15 +1000

    mvc file upload

---
+++ slnMVCImageUpload/Controllers/UploadFormController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace slnMVCImageUpload.Controllers
+{
+    using slnMVCImageUpload.Models;
+
+    public class UploadFormController : Controller
+    {
+     
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult UploadForm(FormModel model)
+        {
+          
+            return Json(Convert.ToString("formuploaded"), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
